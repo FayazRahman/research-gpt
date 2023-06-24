75f6b06e3f10993108b9238d1859b1979df43cb9
FayazRahman <fayazrahman4u@gmail.com>
2023-06-25 02:23:38 +0530

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         extras_require=extras_require,
         packages=find_packages(),
         name=package_name,
-        version="0.0.3",
+        version="0.0.4",
         description="Automated Research. Powered by LoopGPT.",
         entry_points={"console_scripts": ["research = researchgpt.cli:main"]},
     )
